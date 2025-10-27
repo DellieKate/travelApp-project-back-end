6b778b2f531deff3048d3dff8e2fe937bcbb1840
Kate Mendoza <16334@coderacademy.edu.au>
2025-10-27 15:38:24 +1100

    feat!: import bcrypt, and add salt and hash to user password

--- src/database/entities/User.js
+++ src/database/entities/User.js
@@ -1,14 +1,15 @@
 import mongoose from "mongoose";
-//import { validate } from "validator";
-import * as crypto from "crypto";
+import validator from "validator";
+import bcrypt from "bcrypt";
 
-let UserSchema = new mongoose.Schema(
+
+const UserSchema = new mongoose.Schema(
     {
         username: {
             type: String,
             required: true,
             unique: true,
-            minLength: 2
+            minlength: 2
         },
         email: {
             type: String,
@@ -30,7 +31,7 @@ let UserSchema = new mongoose.Schema(
             minlength: 8,
             validate: {
                 validator: function (newPassword) {
-                    let passwordStrengthRules = {
+                    const passwordStrengthRules = {
                         minLength: 8,
                         minLowercase: 1,
                         minUppercase: 1,
@@ -47,23 +48,24 @@ let UserSchema = new mongoose.Schema(
                     return validator.isStrongPassword(newPassword, passwordStrengthRules);
                 },
                 message: validatorError => `${validatorError.value} is a weak password. Try again!`
-            }
+            },
         },
-        salt: {
-            type: String,
-            required: false,
-            default: function () {
-                return crypto.randomBytes(64).toString("hex");
-            }
-        }
-    }
+    },
+        {timestamps: true }
 );
 
-// after salting password, need to hash it for more security before it is saved in the database
-// do it here....
+userSchema.pre ("save", async function (next) {
+    if (!this.isModified("password")) 
+    return next();
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+    
 
-//password is saved!
-
+userSchema.methods.comparePassword = async function (anotherPassword) {
+    return bcrypt.compare(anotherPassword, this.password); 
+};
 
 const UserModel = mongoose.model("User", UserSchema);
 
