5ae6c4c92b649040a110fb15ccd0388b89e93a3a
Kate Mendoza <16334@coderacademy.edu.au>
2026-01-24 15:19:55 +1100

    feat: add jsdocswagger in server and utils folder

---
+++ src/utils/swagger-jsdoc.js
@@ -0,0 +1,15 @@
+const swaggerJsDocOptions = {
+  info: {
+    title: 'TravelApp Backend Project',
+    license: {
+      name: 'MIT',
+    },
+  },
+
+  baseDir: "./",
+  filesPattern: './**/*.js',
+  swaggerIUPath: '/api-docs',
+  exposeSwaggerUI: true,
+  exposeApiDocs: false,
+  apiDocsPAth: '/v3/api-docs',
+}
