36880c1902f2a7088be92e4e815e5d64601b80f5
Kate Mendoza <16334@coderacademy.edu.au>
2026-02-14 11:18:57 +1100

    update database connection

--- src/database/connectionManager.js
+++ src/database/connectionManager.js
@@ -5,7 +5,7 @@ let DatabaseUri;
 console.log("DB Connection Env", process.env.NODE_ENV)
 
 if (process.env.NODE_ENV) {
-  DatabaseUri = process.env.MONGO_URL || "mongodb+srv://katem_db_user:<mysunshine>@cluster0.elmox5y.mongodb.net/travelApp-project-backend"
+  DatabaseUri = process.env.MONGO_URL 
 } else if (process.env.NODE_ENV == "test") {
   DatabaseUri = process.env.MONGO_URL_TEST || "mongodb://127.0.0.1:27017/travelApp_test"
 } else if (process.env.NODE_ENV == "dev") {
