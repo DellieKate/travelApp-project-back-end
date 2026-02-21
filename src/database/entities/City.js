4b04f7789717c5c0fe82989f4956740b4af3601c
Kate Mendoza <16334@coderacademy.edu.au>
2026-02-22 08:16:34 +1100

    Remve city auto populate condition

--- src/database/entities/City.js
+++ src/database/entities/City.js
@@ -27,13 +27,13 @@ const CitySchema = new mongoose.Schema({
 
 // Auto-populate
 function autoPopulateCity (next) {
-    if (this.country) {
+    // if (this.country) {
         this.populate(
             { path: "country", select: "name visaReq currency language -_id", 
                 populate: { path: "vaxReq", select: "vaxReq -_id" }})
             .populate({ path: "activities", select: "name description -_id"})
             .populate({ path: "packingEssentials", select: "season items -_id" });
-    }
+    // }
 };
 
 CitySchema.pre(/^find/, async function () {
