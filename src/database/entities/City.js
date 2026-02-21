efbf3cfbf7b08609e5f6e821b607833fb0d2ba72
Kate Mendoza <16334@coderacademy.edu.au>
2026-02-22 03:02:32 +1100

    Update city entity pre hook

--- src/database/entities/City.js
+++ src/database/entities/City.js
@@ -28,18 +28,17 @@ const CitySchema = new mongoose.Schema({
 // Auto-populate
 function autoPopulateCity (next) {
     if (this.country) {
-        console.log('pre-populating city!')
         this.populate(
             { path: "country", select: "name visaReq currency language -_id", 
                 populate: { path: "vaxReq", select: "vaxReq -_id" }})
             .populate({ path: "activities", select: "name description -_id"})
             .populate({ path: "packingEssentials", select: "season items -_id" });
-    } else {
-        console.log('Skipping pre-population of city fields')
     }
 };
 
-CitySchema.pre(/^find/, autoPopulateCity);
+CitySchema.pre(/^find/, async function () {
+  await autoPopulateCity.call(this);
+});
 
 const CityModel = mongoose.model("City", CitySchema);
 
